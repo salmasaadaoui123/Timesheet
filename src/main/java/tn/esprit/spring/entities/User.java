76e89c2a513c53f1a736af2fdb891f7b57cfed82
salma saadaoui <salma.saadaoui@esprit.tn>
2023-10-04 11:16:23 +0200

    ajt comm 1

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable {
 	@Enumerated(EnumType.STRING)
 	Role role; 
 //test 
-	
+	//Test
 	public User() {	}
 
 	public User(String firstName, String lastName, Date dateNaissance, Role role) {
